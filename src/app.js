a7ab21e802187f5c3ffb4bfbd0ffc19ec624dccd
garsan84 <jgrsan84@gmail.com>
2021-06-08 20:16:11 +0200

    backend test

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import config from './config';
+
+const app = express();
+
+
+export default app
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import app from './app'
+
+app.listen(3000)
+
+
+console.log(5)
+console.log(process.env.TOKEN)
